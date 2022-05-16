db2c8def7c7c36afeca3d7a34dca786beecaffc4
Anas <manasfullstackdeveloper@gmail.com>
2022-05-16 22:01:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Topbox from './pages/homePage/Home';
+function App() {
+  return (
+    <div>
+      <div>
+      <Topbox/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homePage/Home.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import './HomeStyle.css';
+
+export default function Topbox() {
+  return (
+    <div > 
+      <div className='container box' >
+        <div className="col-4 box1">
+          <h1>Title od longer</h1>
+          <h1>fractured of Blog post</h1>
+          <br />
+          <p>Quis quisquam dolores eveniet praesentium aut? Ex mollitia quam possimus cupiditate odio quod ratione ipsam similique. Nam atque possimus voluptatum omnis?</p>
+          <br />
+          <p className='para'>Lorem ipsum dolor sit...</p>
+        </div>
+      </div>
+      <div className="container ahmad">
+      <div className="row ahmad1"> 
+        <div className="col-6 border border-1 ahmad2">
+          <div className="row">
+            <div className="col-8 anas1">
+              <p>
+              <b>World</b>
+              </p>
+              <h3>Featured Post</h3>
+              <p>Now 12</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, sit. elit. Illo, sit.</p>
+              <br />
+              <p><b>Continue Word</b></p>
+            </div>
+            <div className="col d-flex justify-content-center align-items-center anas">
+              <p>Thumbnail</p>              
+            </div>
+          </div>
+        </div>
+        <div className="col-6 border border-1 ahmad2">
+          <div className="row">
+            <div className="col-8 anas1">
+              <p>
+              <b>World</b>
+              </p>
+              <h3>Featured Post</h3>
+              <p>Now 12</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, sit. elit. Illo, sit.</p>
+              <br />
+              <p><b>Continue Word</b></p>
+            </div>
+            <div className="col d-flex justify-content-center align-items-center anas">
+              <p>Thumbnail</p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-8 mirza">
+            <h4><i>Form The Firebx</i></h4>
+            <hr />
+            <h3>Sample Blog Post</h3>
+            <p>March 4/3/2002 <span>year</span></p>
+             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate pariatur nihil numquam quibusdam, placeat nobis reprehenderit nam, officia minus cumque omnis quae ea vero beatae.</p>
+             <hr />
+             <p>Itaque, debitis. Dolorem obcaecati<span>ad quasi dolorum</span>fugit nesciunt accusantium corrupti libero ducimus optio, corporis eveniet unde labore porro minima ut sunt explicabo mollitia earum quo numquam. Optio neque fugit repudiandae impedit numquam sequi debitis voluptate officiis inventore!</p>
+             <p>Lorem ipsum dolor sit amet, <b>consectetur adipisicing elit.</b> Amet esse et eius excepturi optio architecto dolores ullam sapiente minus sit?</p>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis illum dolorem dolor eaque corporis doloribus voluptatibus beatae quisquam? Eaque, dignissimos!</p>
+             <h3>Heading</h3>
+             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus corporis suscipit vel, dolorum rerum soluta. Quis vel maxime neque vitae harum minus tenetur, numquam autem. Vero quibusdam recusandae officiis laboriosam.</p>
+             <h3>Sab-heading</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, rerum.</p>
+             <p>Lorem, ipsum dolor.</p>
+             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis omnis cupiditate exercitationem, non itaque numquam et? Numquam, veniam non.</p>
+             <h3>Sad-heading</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Id eius deserunt explicabo, alias a pariatur. Saepe, vero architecto? Quidem doloremque quia ipsum nihil aliquam sequi quam deserunt, molestiae corporis provident ea veniam harum eum officiis ex dolorum eos tenetur maiores.</p>
+              <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta.</li>
+                <li>Dolor sit amet consectetur adipisicing elit. Dicta.</li>
+                <li>Consectetur adipisicing elit. Dicta.</li>
+              </ul>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic!</p>
+              <ol>
+                <li>Aonsectetur adipisicing elit. Repellat, iste.</li>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, iste.</li>
+                <li>Sit amet consectetur adipisicing elit. Repellat, iste.</li>
+              </ol>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, exercitationem.</p>
+              <br />
+              <h3>Annther Blog post</h3>
+              <p>December 23,2013 by <span>Mirza Anas</span></p>
+              <p>Lorem ipsum dolor sit amet consectetur<span>adipisicing elit. Animi in</span>  consequatur, quam architecto quae repudiandae reprehenderit sit a numquam cum quaerat blanditiis officia dignissimos hic, quo reiciendis perferendis est? Repudiandae.</p>
+              <p>Lorem ipsum dolor sit amet<b> consectetur, adipisicing elit.</b> Deserunt dignissimos consectetur illo eligendi assumenda cum aspernatur maxime sint eaque voluptas?</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. dolor sit amet consectetur Modi, cumque nihil. Ipsa, reprehenderit saepe?</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, incidunt! Facilis architecto illo molestias quis, vel, sunt error quam repudiandae eum quae blanditiis, quibusdam sed!</p>
+              <h3>New fcature</h3>
+              <p>December 23,2013 by <span>Mirza Anas</span></p>
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nisi quae doloribus cum fuga magnam, fugiat eveniet repellendus distinctio exercitationem, adipisci, aspernatur corrupti repellat eaque obcaecati cupiditate eligendi ut praesentium necessitatibus facere voluptatibus doloremque deserunt!</p>
+              <ul>
+                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus?</li>
+                <li>Sit amet consectetur adipisicing elit. Natus?</li>
+                <li>Amet consectetur adipisicing elit. Natus?</li>
+              </ul>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus facilis autem quia, repudiandae repellendus sequi?</p>
+              <br />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nam quis quaerat.</p>
+              <br />
+              <button>Clear</button>
+              <button>Naver</button>
+          </div>
+          <div className="col-4 mirza1">
+            <div className='ali'>
+              <h4>About</h4>
+              <p className='mirza2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime esse fuga delectus vitae laborum rerum, inventore perferendis aliquam labore.</p>
+            </div>
+            <br />
+            <h4>Archivs</h4>
+            <p>March 2014</p>
+            <p>Febuary 2014</p>
+            <p>january 214</p>
+            <p>December 2013</p>
+            <p>Novenber 2013</p>
+            <p>Oqtober 2013</p>
+            <p>September 2013</p>
+            <p>Auguast 2013</p>
+            <p>June 2013</p>
+            <p>July 2015</p>
+            <p>April 2015</p>
+            <p>May 2017</p>
+            <h4>Elsewhere</h4>
+            <p>You tube</p>
+            <p>Faebook</p>
+            <p>Tawiter</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+
